8bd7ee117fb7140ca5f06ee1753d46d9da958c2e
David Moya <3nueves@gmail.com>
2023-10-03 22:33:47 +0200

    add new config

---
+++ ikctl/config_new.py
@@ -0,0 +1,6 @@
+# 1.- Cargar archivo desde la $HOME/.ikctl
+# 2.- Comprobar que existe el directorio $HOME/.ikctl
+# 3.- Si no existe crearlo y añadir el fichero "$HOME/.ikctl/config"
+# 4.- Avisar que no hay ninguna ruta para cargar kits y servers
+# 5.- Si existe el fichero cargarlo
+
