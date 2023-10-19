a53ebdac92f1489394e78bbf6c574ed722afad09
David Moya <3nueves@gmail.com>
2023-10-19 23:14:39 +0200

    fix text help

--- ikctl/main.py
+++ ikctl/main.py
@@ -6,7 +6,7 @@ def create_parser():
     parser = argparse.ArgumentParser(description="tool for install software in remote servers", prog="ikctl")
 
     parser.add_argument("-l", "--list", choices=["kits", "servers", "context"], help="option to list kits, servers or context")
-    parser.add_argument("-i", "--install", help="Install kit selected")
+    parser.add_argument("-i", "--install", help="Select kit to use")
     parser.add_argument("-n", "--name", help="Name of the groups servers")
     parser.add_argument("-p", "--parameter", help="Add parameters")
     parser.add_argument("-s", "--sudo", choices=["sudo"], help="exec from sudo")
