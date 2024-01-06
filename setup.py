188d5dff0e43afc39c61ee3f96315a6203707408
David Moya <3nueves@gmail.com>
2024-01-06 13:45:10 +0100

    change version in setups

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ikctl',
-    version='0.7.1',
+    version='0.7.2',
     description="kit installer on remote servers",
     author="David Moya López",
     author_email="3nueves@gmail.com",
