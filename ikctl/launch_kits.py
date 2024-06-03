028d2fd27675201035f29900a74955d6c00e51ff
David Moya <3nueves@gmail.com>
2024-06-03 09:09:00 +0200

    Delete Abstract class

--- ikctl/launch_kits.py
+++
@@ -1,7 +0,0 @@
-from abc import abstractmethod
-from abc import ABCMeta
-
-class LaunchKits(metaclass=ABCMeta):
-    @abstractmethod
-    def run_kits(self):
-        pass
--- ikctl/local/local_kits.py
+++ ikctl/local/local_kits.py
@@ -1,2 +1,5 @@
- # Run kits in local servers
-    
+""" Module to Run kits in local servers """
+
+class RunLocalKits:
+    """ Class to run kits in locals servers """
+    pass
