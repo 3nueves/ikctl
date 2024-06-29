9b53e04bd2bef95e9d31759e158aea49a2567222
David Moya <3nueves@gmail.com>
2024-06-29 17:09:40 +0200

    upgrade version to v0.3.0

--- ikctl/config/config.py
+++ ikctl/config/config.py
@@ -5,7 +5,7 @@ import sys
 from envyaml import EnvYAML
 from .create_config_files import CreateFolderAndConfigFile
 
-__version__ = "v0.2.16"
+__version__ = "v0.3.0"
 
 class Config():
     """ Manage path kits """
