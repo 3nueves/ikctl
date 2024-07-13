e6841019afa23690a400ca7998244f85a246e5a3
David Moya <3nueves@gmail.com>
2024-07-13 17:38:55 +0200

    update version

--- ikctl/config/config.py
+++ ikctl/config/config.py
@@ -6,7 +6,7 @@ import logging
 from envyaml import EnvYAML
 from .create_config_files import CreateFolderAndConfigFile
 
-__version__ = "v0.4.5"
+__version__ = "v0.4.9"
 
 class Config():
     """ Manage path kits """
