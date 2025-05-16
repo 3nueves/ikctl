287c91c4b92586b204189276506d526ded570670
David Moya <3nueves@gmail.com>
2025-05-16 11:28:11 +0200

    fix f-string issue

--- ikctl/config/config.py
+++ ikctl/config/config.py
@@ -6,7 +6,7 @@ import logging
 from envyaml import EnvYAML
 from .create_config_files import CreateFolderAndConfigFile
 
-__version__ = "v0.6.0"
+__version__ = "v0.6.1"
 
 class Config():
     """ Manage path kits """
